4e0347a7933eafef423a6d6f77b2f65cd462b1d5
DauThuy <dauthithuy99@gmail.com>
2022-02-20 03:42:57 +0700

    20/02: admin-userDetail, fix size img

--- src/main/java/com/nico/store/store/controller/AdminController.java
+++ src/main/java/com/nico/store/store/controller/AdminController.java
@@ -7,14 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/admin")
 public class AdminController {
     @Autowired
     private UserService userService;
 
+
+    @GetMapping("/showDetailInfoUser")
+    public String showDetailInfoUser(@RequestParam("id") long theId, Model model) {
+
+        // get the employee from the service
+        User user = userService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("user", user);
+
+        // send over to our form
+        return "userDetaill";
+    }
 }
